6ec8746e7f1f8d5e12ba80240783fb62dda9fe05
Irina Greseva <iagreseva@gmail.com>
2023-06-16 18:09:27 -0300

    Clase 17 Django

---
+++ mi_primer_django/urls.py
@@ -0,0 +1,32 @@
+"""
+URL configuration for mi_primer_django project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+#from mi_primer_django.views import inicio, segunda_vista, fecha_actual
+from mi_primer_django import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #path('', inicio),
+    path('', views.inicio),
+    #path('segunda-vista/', segunda_vista),
+    path('segunda-vista/', views.segunda_vista),
+    #path('fecha-actual/', fecha_actual)
+    path('fecha-actual/',views.fecha_actual),
+    path('saludar/',views.saludar),
+    path('bienvenida/<str:nombre>/<str:apellido>/',views.bienvenida)
+]
---
+++ mi_primer_django/views.py
@@ -0,0 +1,57 @@
+#from django.http import HttpResponse
+#from datetime import datetime
+
+from django.http import HttpResponse
+from datetime import datetime
+from django.template import Template, Context
+# V1
+#def inicio(request):
+#    return HttpResponse('Hola soy tu inicio') 
+
+
+#def segunda_vista(request):
+#    return HttpResponse('<h1>Soy la segunda vista!</h1>')
+
+#def fecha_actual(request):
+    
+#    fecha = datetime.now()
+    
+#    return HttpResponse(f'<h1>Fecha actual: {fecha} <h1>')
+
+#def saludar(request):
+#    return HttpResponse('Bienvenido/a!!!')
+
+#def bienvenida(request, nombre, apellido):
+#    return HttpResponse(f'Bienvenido/a {nombre.title()} {apellido.title()}!!!')
+
+
+
+def inicio(request):
+    archivo = open (r'C:\Users\Irina\Desktop\Projectos\mi_primer_django\templates\inicio.html', 'r')
+    
+    template = Template(archivo.read())
+    
+    
+    archivo.close()
+    
+    contexto = Context()
+    
+    renderizar_template = template.render(contexto)
+    
+    return HttpResponse(renderizar_template) 
+
+
+def segunda_vista(request):
+    return HttpResponse('<h1>Soy la segunda vista!</h1>')
+
+def fecha_actual(request):
+    
+    fecha = datetime.now()
+    
+    return HttpResponse(f'<h1>Fecha actual: {fecha} <h1>')
+
+def saludar(request):
+    return HttpResponse('Bienvenido/a!!!')
+
+def bienvenida(request, nombre, apellido):
+    return HttpResponse(f'Bienvenido/a {nombre.title()} {apellido.title()}!!!')
